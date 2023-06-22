1d2b47049ba469f8bf6a990e2c6b20706e6d185a
Alibekov_MM <Alibekov_MM@mail.ru>
2023-06-22 15:20:59 +0300

    zxcmotya

---
+++ source/repos/UnitTestProject1/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stydensheskiy
+{
+    public class StudNumberGenerator
+    {
+        public string GetStudNumber(int year, int group, string fio)
+        {
+            string initials = GetInitials(fio);
+            string formattedYear = GetFormattedYear(year);
+
+            string studNumber = $"{formattedYear}.{group}.{initials}";
+
+            return studNumber;
+        }
+
+        private string GetInitials(string fio)
+        {
+            string[] nameParts = fio.Split(' ');
+            string initials = "";
+
+            foreach (string part in nameParts)
+            {
+                initials += part[0];
+            }
+
+            return initials;
+        }
+
+        private string GetFormattedYear(int year)
+        {
+            return year.ToString("D4");
+        }
+    }
+}
---
+++ source/repos/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testStydencheskiy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetStudNumber_ReturnsCorrectStudNumber()
+        {
+            StudNumberGenerator generator = new StudNumberGenerator();
+            int year = 2022;
+            int group = 1;
+            string fio = "Иванов Иван Иванович";
+            string expectedStudNumber = "2022.1.ИИИ";
+
+            
+            string actualStudNumber = generator.GetStudNumber(year, group, fio);
+
+            
+            Assert.AreEqual(expectedStudNumber, actualStudNumber);
+        }
+
+        [TestMethod]
+        public void GetStudNumber_FIOStartsWithUppercase()
+        {
+            
+            StudNumberGenerator generator = new StudNumberGenerator();
+            int year = 2022;
+            int group = 1;
+            string fio = "Иванов Иван Иванович";
+
+            
+            string[] names = fio.Split(' ');
+            foreach (string name in names)
+            {
+                char firstLetter = name[0];
+                Assert.IsTrue(char.IsUpper(firstLetter)); // Проверка на заглавную букву
+            }
+        }
+        [TestMethod]
+        public void GetStudNumber_ReturnsCorrectStudNumber1()
+        {
+            StudNumberGenerator generator = new StudNumberGenerator();
+            int year = 2020;
+            int group = 2;
+            string fio = "Чуйков Матвей Васильевич";
+            string expectedStudNumber = "2020.2.ЧМВ";
+
+
+            string actualStudNumber = generator.GetStudNumber(year, group, fio);
+
+
+            Assert.AreEqual(expectedStudNumber, actualStudNumber);
+        }
+
+    }
+
+    public class StudNumberGenerator
+    {
+        public string GetStudNumber(int year, int group, string fio)
+        {
+            string initials = GetInitials(fio);
+            return $"{year}.{group}.{initials}";
+        }
+
+        private string GetInitials(string fio)
+        {
+            string[] names = fio.Split(' ');
+            string initials = "";
+            foreach (string name in names)
+            {
+                initials += name[0];
+            }
+            return initials.ToUpper();
+        }
+    }
+
+}
